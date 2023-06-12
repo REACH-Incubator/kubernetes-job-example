49c0a7ae62b33de2174de608e5649975a893a82b
Mikel Emaldi <m.emaldi@deusto.es>
2023-06-12 16:05:33 +0200

    Modifying number of epochs

--- mnist_example.py
+++ mnist_example.py
@@ -34,7 +34,7 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=15)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
